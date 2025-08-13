ed556d58ad7cd6048cf47d96233650d4df5a1cf9
abdurrehman0600 <abdurrehmana346@gmail.com>
2025-08-13 17:33:29 +0500

    Tic Tac Toe

---
+++ Projects/Tic Tac Toe/App.js
@@ -0,0 +1,50 @@
+const btns = document.querySelectorAll(".btn");
+const winnerMsg = document.querySelector(".winner");
+const resultBox = document.querySelector(".result");
+
+let turnO = true;
+const winPatterns = [
+  [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+];
+
+btns.forEach((btn) => {
+   btn.addEventListener("click", () => {
+    if(turnO){
+      btn.innerText="O";
+      turnO = false;
+    }
+    else{
+      btn.innerText="X";
+      turnO = true;
+    }
+    btns.disabled = true; 
+     checkWinner()
+  });
+});
+ const showResult = (winner) => {
+ winnerMsg.innerText = `${winner} , is the winner`;
+ resultBox.classList.remove("hide");
+ disable();
+ }
+ const disable = () =>{
+  for(let b of btns){
+    b.disabled = true;
+  }
+ }
+const checkWinner = () => {
+  for (let pattern of winPatterns ){
+ let pos1 = btns[[0]].innerText;
+ let pos2 = btns[[1]].innerText;
+ let pos3 = btns[[2]].innerText;
+
+  if (pos1 !== "" && pos2!== "" && pos3!== "") {
+      if(pos1 === pos2 && pos2 === pos3)
+      {
+       console.log( `${pos1},"is the winner`);
+    showResult(pos1);
+    }
+  }
+}}
+ const resetGame = () =>{
+   turnO = true;
+ }
