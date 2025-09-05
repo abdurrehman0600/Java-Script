d86567bea0d3f97270f2a8340a64b06813773928
abdurrehman0600 <abdurrehmana346@gmail.com>
2025-09-05 19:39:08 +0500

    API done

---
+++ Fetch API's/General Concept/script.js
@@ -0,0 +1,27 @@
+//aplication-programming interface
+const URL = "https://randomuser.me/api";
+const getFacts = async () => {
+ console.log("Loading...")
+ let response = await fetch (URL);
+ console.log(response);
+ let data = await response.json();
+console.log(data);
+}
+
+//by default get req
+/*
+Understanding Terms
+
+AJAX: Asynchronous JavaScript & XML
+
+JSON: JavaScript Object Notation
+- Example: { "name": "John", "age": 25 }
+
+json() method:
+- Returns a second promise that resolves with the result of parsing 
+  the response body text as JSON.
+- Input is JSON (string), output is JavaScript object.
+*/
+
+//request&response
+//mdn documentation
